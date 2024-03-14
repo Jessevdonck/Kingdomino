09ca1a08152aa705f386bc84a73550cc3e271db2
Jordan Bentkowski <jordan.bentkowski@student.hogent.be>
2024-03-14 12:19:38 +0100

    mini change

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -7,7 +7,6 @@ import ui.SpelApplicatie;
 public class StartUp {
 	public static void main(String[] args) {
 		DomeinController dc = new DomeinController();
-		System.out.printf("%s", new TegelGebied().toString());
-		//new SpelApplicatie(dc).start();
+		new SpelApplicatie(dc).start();
 	}
 }
