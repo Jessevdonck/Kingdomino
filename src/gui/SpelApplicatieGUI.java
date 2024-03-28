31c5d03da3730e7e0f522ffbb670faebac392e9a
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2024-03-28 21:45:13 +0100

    Code clean up
    SpelApplicatieGUI clean up, opgesplitst in verschillende klassen

--- src/gui/SpelApplicatieGUI.java
+++ src/gui/SpelApplicatieGUI.java
@@ -1,260 +1,109 @@
 package GUI;
-
-import java.awt.*;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 import java.util.*;
 import java.util.List;
-
-import com.sun.tools.javac.Main;
 import domein.DomeinController;
 import domein.DominoTegel;
-import domein.Spel;
-import domein.Speler;
-import dto.DominoTegelDTO;
 import dto.SpelerDTO;
-import exceptions.*;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import util.Kleur;
+
 
 public class SpelApplicatieGUI {
-    //gui attributen registreerSpeler
-    @FXML
-    private Button registreerBalk = new Button();
-    @FXML
-    private TextField gebruikersnaamBalk = new TextField();
-    @FXML
-    private TextField geboortejaarBalk = new TextField();
-    @FXML
-    private Label wrongLogIn = new Label();
-    //gui attributen startSpel
-    @FXML
-    private ImageView blauwImageView;
-
-    @FXML
-    private ImageView geelImageView;
-
-    @FXML
-    private ListView<SpelerDTO> geselecteerdeSpelers;
-
-    @FXML
-    private ImageView groenImageView;
-
-    @FXML
-    private ListView<String> ongeselecteerdeSpelers;
-
-    @FXML
-    private ImageView roosImageView;
-
-    @FXML
-    private Button verwijderButton;
-
-    @FXML
-    private Button voegToeButton;
-    private ObservableList<SpelerDTO> geselecteerdeSpelersList;
-    private ObservableList<String> ongeselecteerdeSpelersList;
-
-    //gui attributen sceneChange
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
+    private RegistreerSpelerController registreerSpelerController;
+    private SceneSwitchController sceneSwitchController;
+    private SpelController spelController;
+    private SpelerSelectieController spelerSelectieController;
     private final DomeinController dc;
+
+    private ObservableList<String> spelers = FXCollections.observableArrayList();
+    @FXML ListView<String> ongeselecteerdeSpelers;
+
+
     private Scanner input = new Scanner(System.in);
 
+    public SpelApplicatieGUI()
+    {
+        this.dc = new DomeinController();
+        this.registreerSpelerController = new RegistreerSpelerController(dc);
+        this.spelController = new SpelController(dc);
+        this.sceneSwitchController = new SceneSwitchController(new Stage());
+        this.ongeselecteerdeSpelers = new ListView<>();
+        this.spelerSelectieController = new SpelerSelectieController(ongeselecteerdeSpelers);
 
-    public void updateItemsInListViewLeft(ObservableList<String> spelers){
+        SpelerDTO[] alleSpelers = dc.geefAlleSpelers();
 
+        for (SpelerDTO speler : alleSpelers)
+        {
+            spelers.add(speler.gebruikersnaam());
+        }
 
         this.ongeselecteerdeSpelers.setItems(spelers);
     }
 
-    public void laadSpelersInListView()
-    {
 
-        ongeselecteerdeSpelers = new ListView<>();
-
-        SpelerDTO[] spelersArray = dc.geefAlleSpelers();
-        String[] spelerNamen = new String[spelersArray.length];
-        for(int i = 0; i < spelersArray.length - 1; i++){
-            spelerNamen[i] = spelersArray[i].gebruikersnaam();
-        }
-
-
-        ongeselecteerdeSpelersList = FXCollections.observableArrayList(spelerNamen);
-
-        updateItemsInListViewLeft(ongeselecteerdeSpelersList);
-
-    }
-
-    public void kiesSpelers()
+    @FXML
+    public void laadSpelers()
     {
 
     }
-    public SpelApplicatieGUI()
+
+    public void updateSpelersList(ObservableList<String> spelers)
     {
-        this.dc = new DomeinController();
-    }
-    public SpelApplicatieGUI(DomeinController dc) {
-        this.dc = dc;
+        
     }
 
-    public void startSpel()
+    /*-----------------------------------------------------------------------------SPEL CONTROLLER---------------------------------------------------------------*/
+    private void speelBeurt()
     {
-
-        }
-
-
-    private void speelBeurt(){
-        toonTegelLijst(dc.getSpel().geefTweedeKolom());
-        System.out.println("Welke tegel wil je nemen?");
-        int tegel = input.nextInt();
-
-        dc.getVolgordeKoning().remove(0);
+        spelController.speelBeurt();
     }
-    private void toonTegelLijst(List<DominoTegel> lijst){
-        int count = 0;
-        for (DominoTegel tegel : lijst){
-            count++;
-            System.out.printf("%d : %s", count, tegel.toString());
-
-        }
+    private void toonTegelLijst(List<DominoTegel> lijst)
+    {
+        spelController.toonTegelLijst(lijst);
     }
 
-    private void VraagKleur(Kleur[] kleurenarray){
-        int i = 0;
-        for(Kleur kleur : kleurenarray) {
-
-            System.out.printf("%d : %s \n", i+1, kleur);
-            i++;
-        }
+    public void spelSituatie()
+    {
+        spelController.spelSituatie();
     }
 
-
-    public void geefSpelersAlsKeuze(SpelerDTO[] spelers) {
-        for (int i = 0; i <= spelers.length - 1; i++) {
-
-            System.out.printf("%d : %s, %d \t | \t", i+1, spelers[i].gebruikersnaam(), spelers[i].geboortejaar());
-            if(i % 2 == 1) {
-                System.out.println(); }
-        }
+    public void speelRonde()
+    {
+        spelController.speelRonde();
     }
 
-    public void spelSituatie() {
-        HashMap<SpelerDTO, Kleur> spelers = dc.getSpelendeSpelers();
-        for (SpelerDTO speler : spelers.keySet()) {
-            System.out.println(speler.gebruikersnaam() + " speelt met kleur " + spelers.get(speler));
-        }
-
-        // TODO - Zijn koninkrijk ;Zijn koning op een dominotegel in de startkolom of de eindkolom
-
-        List<DominoTegelDTO> beschikbareTegels = dc.getStartKolom();
-        System.out.println("Beschikbare tegels:");
-        for (DominoTegelDTO tegel : beschikbareTegels) {
-            System.out.printf("%d", tegel.tegelNummer());
-        }
-
-        List<DominoTegelDTO> startkolom = dc.getStartKolom();
-        System.out.println("Startkolom:");
-        for (DominoTegelDTO tegel : startkolom) {
-            System.out.printf("%s : %s", tegel.landschapType1().toString(), tegel.landschapType2().toString());
-        }
-
-        List<DominoTegelDTO> tweedekolom = dc.getTweedeKolom();
-        System.out.println("TweedeKolom:");
-        for (DominoTegelDTO tegel : tweedekolom) {
-            System.out.printf("%s : %s", tegel.landschapType1().toString(), tegel.landschapType2().toString());
-        }
-
-        // TODO - implement SpelApplicatie.spelSituatie
+    /*---------------------------------------------------------------------------REGISTREER SPELER-------------------------------------------------------------*/
+    @FXML
+    public void registreerSpeler()
+    {
+        registreerSpelerController.registreerSpeler();
     }
 
-    public void speelRonde() {
-        // TODO - implement SpelApplicatie.speelRonde
-        speelBeurt();
-        if (dc.isEindeSpel()) {
-            System.out.println("Het spel is afgelopen.");
-        }
+    /*-----------------------------------------------------------------------------SCENE SWITCH---------------------------------------------------------------*/
+    @FXML
+    public void switchToRegisterScene(ActionEvent event) throws IOException
+    {
+       sceneSwitchController.switchToRegisterScene(event);
     }
 
-    public void registreerSpeler() {
-
-
-        String gebruikersnaam = gebruikersnaamBalk.getText();
-
-        String geboortejaarString = geboortejaarBalk.getText();
-        try {
-            if (geboortejaarString.trim().isBlank())
-                throw new OntbrekendGeboortejaarException();
-        } catch (OntbrekendGeboortejaarException | NumberFormatException e) {
-            wrongLogIn.setText(e.getMessage());
-            wrongLogIn.setStyle("-fx-text-fill: red;");
-        }
-
-        int geboortejaar = Integer.parseInt(geboortejaarBalk.getText());
-
-
-
-        try {
-            dc.registreerSpeler(gebruikersnaam, geboortejaar);
-            wrongLogIn.setText("Speler registreren succesvol!");
-            wrongLogIn.setStyle("-fx-text-fill: green;");
-
-        } catch (GebruikersnaamInGebruikException | TeJongeGebruikerException | OngeldigeGebruikersnaamException | SpatiesInGebruikersnaamException |
-                 OntbrekendeGebruikersnaamException | IllegalArgumentException e)
-        {
-            wrongLogIn.setText(e.getMessage());
-            wrongLogIn.setStyle("-fx-text-fill: red;");
-        }
+    public void switchToHomescreen(MouseEvent event) throws IOException
+    {
+        sceneSwitchController.switchToHomescreen(event);
     }
 
-    public void switchToRegisterScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    public void switchToSpeelScene(ActionEvent event) throws IOException
+    {
+        sceneSwitchController.switchToSpeelScene(event);
     }
-
-    public void switchToHomescreen(MouseEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Homepage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-
-    public void switchToSpeelScene(ActionEvent event) throws IOException {
-
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/spelersKiezen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-        SpelApplicatieGUI spelApplicatieGUI = new SpelApplicatieGUI();
-
-        //spelApplicatieGUI.laadSpelersInListView();
-}
 
 
     public void afsluiten(ActionEvent event) {
